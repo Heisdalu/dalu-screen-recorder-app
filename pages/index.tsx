3f6670c8c636148bce09b2697caa558f077e90f3
divine <divineobi07@gmail.com>
2023-10-01 00:36:04 +0100

    add css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,105 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter, Sora, Work_Sans } from "next/font/google";
+import Wrapper from "@/components/Wrapper/Wrapper";
+import DefaultImage from "@/components/Default/DefaultImage";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  adjustFontFallback: false,
+});
+const sora = Sora({
+  subsets: ["latin"],
+  display: "swap",
+  adjustFontFallback: false,
+});
+const work_sans = Work_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  adjustFontFallback: false,
+});
+
+// <div className={``}></div>;
+// const gee = {`${sora.className}`}
+// const gee = {`${work_sans.className}`}
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className={inter.className}>
+      <Wrapper>
+        <nav className="border-1 flex justify-between">
+          <div className="space-x-0.5 flex elemVertical border-1">
+            <DefaultImage
+              src="/logo.svg"
+              height={40}
+              width={40}
+              className="jusit"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+            <span className="text-[1rem] font-[700] text-[#100A42]">
+              HelpMeOut
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div
+            className={`hidden md:flex ${work_sans.className} elemVertical space-x-[2.4375rem] text-[1rem] font-[500] text-black`}
+          >
+            <h1>Feature</h1>
+            <h1>How it works</h1>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+          <button
+            className={`${sora.className} font-[600] text-[1rem] md:text-[1.125rem] text-darkBlue`}
+          >
+            Get Started
+          </button>
+        </nav>
+        <header className="md:grid vlg:gallery vlg:py-[5rem]">
+          <div className="max-w-[500px] md:max-w-[auto]">
+            <h1
+              className={`${sora.className} leading-[100%] space-y-[10px] text-[1.5rem] flex flex-col mb-[1rem] md:text-[2.5rem] md:mb-[1.25rem] vlg:text-[4rem]  border-1`}
+            >
+              <span>Show Them</span>
+              <span>Don’t Just Tell</span>
+            </h1>
+            <p className="mb-[2rem] text-[1rem] md:text-[1.25rem] md:mb-[2.5rem] vlg:mb-[3rem]">
+              Help your friends and loved ones by creating and sending videos on
+              how to get things done on a website.
+            </p>
+            <button className="leading-[1.75rem] rounded-[0.5rem] bg-darkBlue text-white elemVertical py-[1.38rem] px-[1.5rem]">
+              <span>Install HelpMeOut</span>
+              <span>
+                <DefaultImage src="/arrowLeft.svg" height={21} width={20} />
+              </span>
+            </button>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <div className="py-2 border-1 space-y-1 fiex flex-col lg:py-[0]  lg:space-y-[0] justify-center imageGallery ">
+            <div className="grid lg:pr-[1.5rem] border-1">
+              <DefaultImage
+                height={300}
+                width={300}
+                src="/adobe1.png"
+                className="mx-auto rounded-[0.5rem]"
+              />
+              <DefaultImage
+                height={300}
+                width={300}
+                src="/adobe2.png"
+                className="pt-[1rem] mx-auto lg:self-end lg:pt-[0] rounded-[0.5rem]"
+              />
+            </div>
+
+            <div>
+              <DefaultImage
+                height={300}
+                width={300}
+                src="/adobe3.png"
+                className="mx-auto rounded-[0.5rem]"
+              />
+            </div>
+          </div>
+        </header>
+      </Wrapper>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    spacing: {
+      0.5: "0.5rem",
+      0.75: "0.75rem",
+      1: "1rem",
+      1.5: "1.5rem",
+      2: "2rem",
+      16: "1rem",
+      100: "100%",
+    },
+    screens: {
+      md: "768px",
+      lg: "1024px",
+      vlg: "1280px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white: "#fff",
+        black: "#000",
+        darkBlue: "#120B48",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      borderColor: {
+        1: "#000",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
